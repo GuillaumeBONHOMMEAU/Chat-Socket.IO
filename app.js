ca9a6bbf76b5628d43e493c1f2fb3bd95e50fef4
Doshibu <guillaume.bonhommeau@viacesi.fr>
2017-03-08 21:33:32 +0100

    Add public dir and emoji lib

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-var app = require('express')(),
+var express=require('express'),
+    app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
     ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
-    fs = require('fs');
+    fs = require('fs'),
+    port = process.env.PORT || 8080;
+
+//Run the server
+server.listen(port, function () {
+    console.log('Server listening at port %d', port);
+});
 
 // Chargement de la page index.html
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendfile(__dirname + '/public/index.html');
 });
 
+app.use(express.static(__dirname + '/public'));
+
 io.sockets.on('connection', function (socket, pseudo) {
+    console.log("New connection");
+
     // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
     socket.on('nouveau_client', function(pseudo) {
         pseudo = ent.encode(pseudo);
