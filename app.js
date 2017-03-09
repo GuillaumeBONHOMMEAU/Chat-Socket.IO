c12fb71bfa821d33f66cbf76aa052fec5e72a3e1
Doshibu <guillaume.bonhommeau@viacesi.fr>
2017-03-09 21:41:40 +0100

    Add randpass script and modal login

--- app.js
+++ app.js
@@ -4,35 +4,43 @@ var express=require('express'),
     io = require('socket.io').listen(server),
     ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
     fs = require('fs'),
+    rd = require('./randpass'),
     port = process.env.PORT || 8080;
 
-//Run the server
+// Run the server
 server.listen(port, function () {
     console.log('Server listening at port %d', port);
 });
 
-// Chargement de la page index.html
+// Load index.html page
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/public/index.html');
 });
 
+// Set the default path root of internal ressources to /public
 app.use(express.static(__dirname + '/public'));
 
 io.sockets.on('connection', function (socket, pseudo) {
     console.log("New connection");
 
-    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+    // As soon as we get a new client name, we stock it and inform others by broadcast
     socket.on('nouveau_client', function(pseudo) {
-        pseudo = ent.encode(pseudo);
+        //pseudo = ent.encode(pseudo);
+        pseudo = rd.randpass(8);
         socket.pseudo = pseudo;
         socket.broadcast.emit('nouveau_client', pseudo);
     });
 
-    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+    // As soon as we get message, we get pseudo and send message to others by broadcast
     socket.on('message', function (message) {
-        message = ent.encode(message);
+        // !!! Prevent smiley to display correctly !!!
+        //message = ent.encode(message); Encode messgae
         socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
     }); 
+});
+
+app.post('/test', function (req, res) {
+    return rd.randpass(8);
 });
 
 server.listen(8080);
